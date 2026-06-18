e427a18f8eadb1bbf6b2447f0b10b398afecb72c
nidhi192004 <thakrenidhi41@gmail.com>
2026-06-18 14:10:42 +0530

    1

---
+++ practice Day-1.py
@@ -0,0 +1,51 @@
+# a=9
+# if (a>=18):
+#     print("You are eligible to vote.")
+# else:
+#     print("You are not eligible to vote.")
+
+# a=int(input("enter your number:"))
+# if (a%2==0):
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+# a=int(input("enter your number:"))
+# if (a>0):
+#     print("The number is positive.")
+# elif (a<0):
+#     print("The number is negative.")
+# else:
+#     print("The number is zero.")
+
+# find the largest number in the list without using max() function
+# arr=[81,455,234,5, 27, 9,589, 234, 5, 27,9,89]
+# max=arr[0]
+# for i in arr:
+#     if (i>max):
+#         max=i
+# print("The maximum number is:", max)
+
+# find the largest number in the list using max() function
+arr=[81,455,234,5, 27, 9,589, 234, 5, 27,9,89]
+largest=arr[0]
+for num in arr:
+    if num>largest:
+        largest=num 
+print("The largest number is:", largest)
+
+
+# print number from 1 to 100 
+# for i in range(1, 10):
+#     print(i)
+
+
+# print even numbers from 1 to 25
+# for i in range (1,25):
+#     if (i%2==0):
+#         print(i)
+
+# arr=[81,455,234,5, 27, 9,589, 234, 5, 27,9,89]
+# # find the largest number in the list using max() function
+# largest=max(arrv)
+# print("The largest number is:", largest)
