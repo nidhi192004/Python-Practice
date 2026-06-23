6de8ad57991eb14b6d3c7c135cb052df6e04caf7
nidhi192004 <thakrenidhi41@gmail.com>
2026-06-23 14:16:49 +0530

    2

---
+++ practice Day-2.py
@@ -0,0 +1,45 @@
+#program to find factorial
+# num = int(input("Enter a number: "))
+# factorial = 1
+# for i in range(1, num + 1):
+#     factorial=factorial * i
+# print("The factorial of", num, "is", factorial)
+
+# program to find the prime number
+# num=int(input("Enter a number: "))
+# count=0
+# for i in range(1, num + 1):
+#     if (num % i == 0):
+#         count += 1
+# if (count == 2):
+#     print(num, "is a prime number.")
+
+
+# program to reverse a number
+# num=int(input("Enter a number: "))
+# reverse=0
+# while (num>0):
+#     digit=num%10
+#     reverse=reverse*10+digit
+#     num=num//10
+# print("The reverse of the number is:", reverse)
+
+
+
+# program for sum of digits in a number
+# num=int(input("Enter a number: "))
+# sum=0
+# while (num>0):
+#     digit=num%10
+#     sum=sum+digit
+#     num=num//10
+# print("The sum of digits in the number is:", sum)
+
+
+# program for count of digits in a number
+# num=int(input("Enter a number: "))
+# count=0
+# while (num>0):
+#     num=num//10
+#     count += 1
+# print("The count of digits in the number is:", count)
