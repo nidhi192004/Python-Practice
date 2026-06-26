b57a727040b862f899dc8607908ed581f2bcaf57
nidhi192004 <thakrenidhi41@gmail.com>
2026-06-26 21:07:50 +0530

    3

---
+++ Practice Day-3.py
@@ -0,0 +1,87 @@
+# To print star pattern
+# for i in range (1,8):
+#     print("*" * i)
+
+# To print Number pattern
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
+
+# To print Multiplication Table.
+# num=int(input("Enter the number"))
+# for i in range(1,11):
+#     print(num, "X", i, "=" ,num*i)
+
+# To check a armstrong number 
+# num=int(input("Enter a number"))
+# original=num
+# sum=0
+# while num>10:
+#     digit=num%10
+#     sum=sum+(digit**3)
+#     num = num // 10
+# if original==sum:
+#     print("Armstrong number")
+# else:
+#     print("not an armstrong number")
+
+
+# To check palindrome number
+# num=int(input("enter a number:"))
+# original=num
+# reverse=0
+# while num>0:
+#     digit= num%10
+#     reverse=reverse*10+digit
+#     num=num//10
+# if original==reverse:
+#     print("Palindrome")
+# else:
+#     print("Not a palindrome")
+
+
+# To check sum of digits
+# num=int(input("Enter your number:"))
+# total=0
+# while num>0:
+#     digit=num%10
+#     total=total+digit
+#     num=num//10
+# print ("Sum of the numbers=",total)
+
+
+# To Print count of digits.
+# num=int(input("Enter your number:"))
+# count=0
+# while num>0:
+#      count+=1
+#      num=num//10
+# print ("Sum of the numbers=",count)
+
+
+
+# To print the reverse number 
+# num=(input("Enter your Number:"))
+# reverse=0
+# while num>0:
+#     digit= num%10
+#     reverse=reverse*10+digit
+#     num=num//10
+# print("the reverse number is=",reverse)
+
+
+# Program to reverse a string.
+# text=input("Enter your string:")
+# reverse=text[::-1]
+# print("reverse string=", reverse)
+
+
+# To print whether the string is palindrome or not.
+# text=input("Enter a string:")
+# reverse=text[::-1]
+# if text==reverse:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
